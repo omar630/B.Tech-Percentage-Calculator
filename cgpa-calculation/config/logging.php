db6c9a4fc30a0925837c1eb00e99f3951cb91893
Mohammed Omar <omarmd2311@gmail.com>
2021-04-23 19:50:03 +0530

    added log viewer

--- cgpa-calculation/config/logging.php
+++ cgpa-calculation/config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'larabug'],
+            'channels' => ['daily', 'larabug'],
             'ignore_exceptions' => false,
         ],
 
